387aed117062d4436944695bec0ece0a88e094a8
x-magic <xmagic5589@gmail.com>
2021-04-03 20:39:34 +1100

    Misc: Added update script for windowsdesktop-runtime*

--- misc/updateall.py
+++ misc/updateall.py
@@ -1,9 +1,15 @@
+import os
+
+os.chdir(os.path.abspath(os.path.dirname(__file__)))
+
 updaters = [
-	'busybox',
-	'lessmsi',
-	'nmap-portable'
+    'busybox',
+    'lessmsi',
+    'nmap-portable',
+    'windowsdesktop-runtime',
+    'windowsdesktop-runtime-lts-3.1.x'
 ]
 
 for updater in updaters:
-	print("===> {0}".format(updater))
-	exec(open('update-' + updater + '.py').read())
+    print("===> {0}".format(updater))
+    exec(open('update-' + updater + '.py').read())
