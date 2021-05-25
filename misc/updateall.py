579fa58e25c4667eeb54b107a6745aa7ba3cd391
x-magic <xmagic5589@gmail.com>
2021-05-26 03:02:22 +1000

    Updated misc scripts

--- misc/updateall.py
+++ misc/updateall.py
@@ -4,8 +4,6 @@ os.chdir(os.path.abspath(os.path.dirname(__file__)))
 
 updaters = [
     'busybox',
-    'lessmsi',
-    'nmap-portable',
     'windowsdesktop-runtime',
     'windowsdesktop-runtime-lts-3.1.x'
 ]
