7321466fcb6c4d982436827d443108a6bc2c6ded
x-magic <xmagic5589@gmail.com>
2021-06-22 15:20:14 +1000

    Misc: updated .NET runtime version check patterns

--- misc/update-windowsdesktop-runtime-lts-3.1.x.py
+++ misc/update-windowsdesktop-runtime-lts-3.1.x.py
@@ -12,7 +12,7 @@ for arch in archs:
     manifest = json.load(open(arch_manifest_path, mode='r'))
 
     with urllib.request.urlopen(version_check_url) as url:
-        version_current = re.findall('<h2 class="h3" id="([\\d.]+)"', url.read().decode())[0]
+        version_current = re.findall('data-target="#version_0">([\\d.]+)</button>', url.read().decode())[0]
         version_existing = manifest['version']
 
         if version_current != version_existing:
